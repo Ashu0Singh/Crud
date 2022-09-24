857083cf204071573593010d63e19a1c33b39b51
Ashu0Singh <ashutosh.20bci7070@vitap.ac.in>
2022-09-24 23:52:27 +0530

    Practice Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import SignUp from "./components/SignUp";
+
 function App() {
   return (
     <div className="App">
+      <SignUp />
     </div>
   );
 }
---
+++ src/components/SignUp/index.js
@@ -0,0 +1,9 @@
+import './index.css'
+
+export default function SignUp(){
+    return (
+        <div className='box-container'>
+            <h2 className='fs-600 fc-white c'>Create an account</h2>
+        </div>
+    )
+}
