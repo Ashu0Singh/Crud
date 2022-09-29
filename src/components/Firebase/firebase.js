df30e765fdc3290f824586e4464d265adbaf0c1c
Ashu0Singh <ashutosh.20bci7070@vitap.ac.in>
2022-09-29 11:46:30 +0530

    Changed to development server

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from '@firebase/firestore';
 
 const firebaseConfig = {
-  apiKey            : process.env.REACT_PRO_APIKEY,
-  authDomain        : process.env.REACT_PRO_AUTHDOMAIN,
-  projectId         : "crud-production-1",
-  storageBucket     : process.env.REACT_PRO_STORAGEBUCKET,
-  messagingSenderId : process.env.REACT_PRO_MESSAGING_SENDING_ID,
-  appId             : process.env.REACT_PRO_APIID,
-  measurementId     : process.env.REACT_PRO_MEASUREMENT_ID
+  apiKey            : process.env.REACT_APIKEY,
+  authDomain        : process.env.REACT_AUTHDOMAIN,
+  projectId         : "crud-development-0",
+  storageBucket     : process.env.REACT_STORAGEBUCKET,
+  messagingSenderId : process.env.REACT_MESSAGING_SENDING_ID,
+  appId             : process.env.REACT_APIID,
+  measurementId     : process.env.REACT_MEASUREMENT_ID
 };
 
 
