b3e00ff025f7650e5b2b0608d50ca3145564a7a0
Ashu0Singh <ashutosh.20bci7070@vitap.ac.in>
2022-09-29 11:36:12 +0530

    Connected to production server

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -2,13 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from '@firebase/firestore';
 
 const firebaseConfig = {
-  // apiKey: "AIzaSyAJu5MIWAdqQJom6C5SGe4SZ6K3r2iXMuY",
-  // authDomain: "crud-production-1.firebaseapp.com",
-  // projectId: "crud-production-1",
-  // storageBucket: "crud-production-1.appspot.com",
-  // messagingSenderId: "1089150464700",
-  // appId: "1:1089150464700:web:c1886cff674d6362d056b9",
-  // measurementId: "G-CVWKQFRDDH"
   apiKey            : process.env.REACT_PRO_APIKEY,
   authDomain        : process.env.REACT_PRO_AUTHDOMAIN,
   projectId         : "crud-production-1",
